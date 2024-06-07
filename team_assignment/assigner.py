6ed82e872d186ac2b133da5a4a0a3943cb194b55
Parth Bhangla <parthbhangla987@gmail.com>
2024-06-07 18:11:52 +0530

    correction

--- team_assignment/assigner.py
+++ team_assignment/assigner.py
@@ -15,7 +15,7 @@ class TeamAssigner:
         image_2d = image.reshape(-1, 3)
 
         # Perfomring KMeans Clustering for 2 clusters
-        kmeans = KMeans(n_clusters=2, init='k-means++', n_init=1)
+        kmeans = KMeans(n_clusters=2, init='k-means++', n_init=10)
         kmeans.fit(image_2d)
 
         return kmeans
